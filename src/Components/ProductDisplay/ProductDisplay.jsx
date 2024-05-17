5c07f53521888d84c24bb9f634577ea5d2f09bbf
Priya Chauhan <Priya@123>
2024-05-18 02:27:29 +0530

    created cart

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './ProductDisplay.css';
 import star_icon from "../Assets/star_icon.png";
 import star_dull_icon from "../Assets/star_dull_icon.png";
+import { ShopContext } from '../../Context/ShopContext';
 
 const ProductDisplay = (props) => {
     const {product} = props;
+    const {addToCart}=useContext(ShopContext);
   return (
     <div className='productdisplay'>
         <div className="productdisplay-left">
@@ -30,8 +32,24 @@ const ProductDisplay = (props) => {
             </div>
             <div className="productdisplay-right-prices">
                 <div className="productdisplay-right-price-old">${product.old_price}</div>
-                <div className="productdisplay-right-price-old">${product.new_price}</div>
+                <div className="productdisplay-right-price-new">${product.new_price}</div>
             </div>
+            <div className="productdisplay-right-description">
+                A pullover shirt, close fitting and a round neckline and short sleeves
+            </div>
+            <div className="productdisplay-right-size">
+                <h1>Select Size</h1>
+                <div className="productdisplay-right-sizes">
+                    <div>S</div>
+                    <div>M</div>
+                    <div>L</div>
+                    <div>XL</div>
+                    <div>XXL</div>
+                </div>
+            </div>
+            <button onClick={()=>{addToCart(product.id)}}>ADD TO CART</button>
+            <p className='productdisplay-right-category'><span>Category : </span>Women, T-shirt,, Crop Top</p>
+            <p className='productdisplay-right-category'><span>Tags : </span>Modern, Latest</p>
         </div>
     </div>
   )
